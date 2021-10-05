ecc2ae43039cfc278a54b5f1fa6d96c9ca3a464a
Bots.Business <git.sync@bots.business>
2021-10-05 22:12:16 +0000

    Bots.Business export for bot id: 460564

---
+++ commands/_iniciar.js
@@ -0,0 +1,23 @@
+/*CMD
+  command: /iniciar
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+Olá, eu sou a *DUDA*. Uma robô automatizado da equipe *WolkTeen Dev*. Fui criada para gerenciar as vendas/suportes da equipe 🤖
+
+Digite /planos para *ver e comprar os planos disponíveis* ✅
+
+Digite /comprovantes para enviar *prova de pagamentos* 🧾
+
+Digite /suporte para enviar *mensagem de apoio* 🗣️
+
+_Versão do BOT_: *1.0*
+_Última Atualização_: *05/10/2021 às 14:54 Brasília*
+  ANSWER
+  keyboard: 
+  aliases: 
+CMD*/
+
